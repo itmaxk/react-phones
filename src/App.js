80bd525042450c643897d8f556f87c6c88968b3b
Kurgansky Maxim <it.maxk@gmail.com>
2022-10-08 19:22:46 +0300

    React phones

--- src/App.js
+++ src/App.js
@@ -1,23 +1,91 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="wrapper clear">
+      <header className="d-flex justify-between align-center p-40">
+        <div className="d-flex align-center">
+          <img className="logo" src="/img/logo.png" alt="logo" />
+          <div className="headerInfo">
+            <h3 className="text-uppercase">React Phones</h3>
+            <p className="opacity-5">Магазин лучших телефонов</p>
+          </div>
+        </div>
+
+        <ul className="d-flex">
+          <li className="mr-30">
+          <img className="cart" src="/img/cart.svg" alt="cart" />
+            <span>1205 руб.</span>
+          </li>
+
+          <li>
+          <img className="user" src="/img/user.svg" alt="user" />
+          </li>
+        </ul>
       </header>
+
+      <div className="content p-40">
+        <h1 className="mb-40">Все телефоны</h1>
+        
+        <div className="d-flex">
+          <div className="card">
+            <img className="phone" src="/img/phones/1.jpg" alt="Phone" />
+            <h5>Смартфон Samsung Galaxy A23 4/64GB персиковый (A235)</h5>
+            <div className="d-flex justify-between align-center">
+              <div className="d-flex flex-column">
+                <span>Цена:</span>
+                <b>19 990 руб.</b>
+              </div>
+              <button className="button">
+                <img src="/img/plus.svg" alt="Plus" />
+              </button>
+            </div>
+          </div>
+
+          <div className="card">
+            <img className="phone" src="/img/phones/2.jpg" alt="Phone" />
+            <h5>Смартфон Samsung Galaxy A23 4/64GB персиковый (A235)</h5>
+            <div className="d-flex justify-between align-center">
+              <div className="d-flex flex-column">
+                <span>Цена:</span>
+                <b>19 990 руб.</b>
+              </div>
+              <button className="button">
+                <img src="/img/plus.svg" alt="Plus" />
+              </button>
+            </div>
+          </div>
+
+          <div className="card">
+            <img className="phone" src="/img/phones/3.jpg" alt="Phone" />
+            <h5>Смартфон Samsung Galaxy A23 4/64GB персиковый (A235)</h5>
+            <div className="d-flex justify-between align-center">
+              <div className="d-flex flex-column">
+                <span>Цена:</span>
+                <b>19 990 руб.</b>
+              </div>
+              <button className="button">
+                <img src="/img/plus.svg" alt="Plus" />
+              </button>
+            </div>
+          </div>
+
+          <div className="card">
+            <img className="phone" src="/img/phones/4.jpg" alt="Phone" />
+            <h5>Смартфон Samsung Galaxy A23 4/64GB персиковый (A235)</h5>
+            <div className="d-flex justify-between align-center">
+              <div className="d-flex flex-column">
+                <span>Цена:</span>
+                <b>19 990 руб.</b>
+              </div>
+              <button className="button">
+                <img src="/img/plus.svg" alt="Plus" />
+              </button>
+            </div>
+          </div>
+        </div>
+
+
+
+      </div>
     </div>
   );
 }
